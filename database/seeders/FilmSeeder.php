fcb6effeea27e7dd60de6a4aec3ddeaff5338495
NWemphy <keraf16dalopez@gmail.com>
2025-06-19 02:30:12 +0700

    menambahkan seeders Filmseeder

--- database/seeders/FilmSeeder.php
+++ database/seeders/FilmSeeder.php
@@ -1,26 +1,43 @@
 <?php
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Film;
-use App\Helpers\FilmData;
 
 class FilmSeeder extends Seeder
 {
     public function run()
     {
-        $films = FilmData::all();
+        $films = [
+            [
+                'title' => 'Everything Everywhere All At Once',
+                'year' => 2022,
+                'poster' => 'https://example.com/poster1.jpg',
+                'description' => 'Seorang ibu imigran terjebak dalam multiverse absurd dan harus menyelamatkan dunia sambil memperbaiki hubungannya dengan keluarga.',
+                'director' => 'Daniel Kwan, Daniel Scheinert',
+                'duration' => '139', // dalam menit
+            ],
+            [
+                'title' => 'Spider-Man: Across the Spider-Verse',
+                'year' => 2023,
+                'poster' => 'https://example.com/poster2.jpg',
+                'description' => 'Miles Morales berpetualang ke semesta lain dan bertemu para Spider-People untuk menghentikan ancaman besar.',
+                'director' => 'Joaquim Dos Santos, Kemp Powers, Justin K. Thompson',
+                'duration' => '140',
+            ],
+            [
+                'title' => 'Oppenheimer',
+                'year' => 2023,
+                'poster' => 'https://example.com/poster3.jpg',
+                'description' => 'Kisah kehidupan J. Robert Oppenheimer, fisikawan di balik proyek bom atom.',
+                'director' => 'Christopher Nolan',
+                'duration' => '180',
+            ],
+            // Tambah film lainnya sesuai kebutuhan
+        ];
 
         foreach ($films as $film) {
-            Film::create([
-                'title' => $film['title'],
-                'year' => $film['year'],
-                'poster' => $film['poster'],
-                'description' => $film['description'],
-                'director' => $film['director'],
-                'duration' => $film['duration'],
-            ]);
+            Film::create($film);
         }
     }
 }
