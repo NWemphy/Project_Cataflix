a809a5404011b9a32acff2e2d7148b49629d3519
Cia <vincentciatanti.com>
2025-06-19 20:59:28 +0700

    memperbaiki routes untuk review dan watchlist

--- routes/web.php
+++ routes/web.php
@@ -44,14 +44,12 @@ Route::get('/film/{id}', function ($id) {
     return view('film-detail', ['film' => $film]);
 })->middleware('auth')->name('film.detail');
 
-//Review film
-Route::middleware(['auth'])->group(function () {
-    Route::post('/films/{movie}/reviews', [ReviewController::class, 'store'])->name('reviews.store');
-    Route::get('/reviews/{review}/edit', [ReviewController::class, 'edit'])->name('reviews.edit');
-    Route::put('/reviews/{review}', [ReviewController::class, 'update'])->name('reviews.update');
-    Route::delete('/reviews/{review}', [ReviewController::class, 'destroy'])->name('reviews.destroy');
-    
-    Route::get('/watchlist', [WatchlistController::class, 'index'])->name('watchlist.index');
-    Route::post('/watchlist/{film}', [WatchlistController::class, 'store'])->name('watchlist.store');
-    Route::delete('/watchlist/{film}', [WatchlistController::class, 'destroy'])->name('watchlist.destroy');
-});
+// REVIEW
+Route::post('/films/{film}/reviews', [ReviewController::class, 'store'])->middleware('auth')->name('reviews.store');
+Route::delete('/reviews/{id}', [ReviewController::class, 'destroy'])->middleware('auth')->name('reviews.destroy');
+
+// WATCHLIST
+Route::get('/watchlist', [WatchlistController::class, 'index'])->middleware('auth')->name('watchlist.index');
+Route::post('/watchlist/{film}', [WatchlistController::class, 'store'])->middleware('auth')->name('watchlist.store');
+Route::delete('/watchlist/{id}', [WatchlistController::class, 'destroy'])->middleware('auth')->name('watchlist.destroy');
+
